e4bdbbe581b9ab6f7334102e368e541e0f1f6f95
ivgtr <ivgtr.me@gmail.com>
2026-06-28 02:58:52 +0900

    clarify size input props

--- src/components/molecules/SizeInput/index.tsx
+++ src/components/molecules/SizeInput/index.tsx
@@ -1,23 +1,29 @@
 import classNames from "classnames";
 import React, { memo } from "react";
+import type { ChangeEvent } from "react";
 
 const sizeArray = [...new Array(50)].map((_, i) => i + 1);
 
 export const SizeInput = memo(
   ({
-    handleChange,
-    value,
+    onValueChange,
+    defaultValue,
     id,
+    ariaDescribedBy,
   }: {
-    handleChange: (size: string) => void;
-    value: string;
+    onValueChange: (size: string) => void;
+    defaultValue: string;
     id: string;
+    ariaDescribedBy?: string;
   }) => {
     return (
       <select
         id={id}
-        defaultValue={value}
-        onChange={(e) => handleChange(e.currentTarget.value)}
+        defaultValue={defaultValue}
+        aria-describedby={ariaDescribedBy}
+        onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+          onValueChange(e.currentTarget.value)
+        }
         className={classNames("h-12", "px-4", "py-2", "border-2", "border-gray-600", "rounded-lg")}
       >
         {sizeArray.map((item, index) => {
